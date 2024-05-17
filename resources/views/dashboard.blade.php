3803c0217095a8fab59a3a6aaee3f970dd967ac6
LuisLuna24 <eduarlun4@gmail.com>
2024-05-17 12:56:46 -0600

    Correccion de alertas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <h1 class="text-center text-8xl font-bold text-white">Proximamente</h1>
             </div>
         </div>
     </div>
